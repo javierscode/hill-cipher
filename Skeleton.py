3b54b506bad2d9d6d46ee25a6d0aca2d260eab48
soyjavierlinares <javihp79@gmail.com>
2021-03-11 16:09:58 +0100

    Initial structure

---
+++ Skeleton.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+# --- IMPLEMENTATION GOES HERE -----------------------------------------------
+#  Student helpers (functions, constants, etc.) can be defined here, if needed
+
+import random
+
+
+
+
+# ----------------------------------------------------------------------------
+
+
+
+
+
+def uoc_hill_genkey(size):
+    """
+    EXERCISE 1: Hill Key Generation
+    :size: matrix size
+    :return: size x size matrix containing the key
+    """
+
+    matrix = []
+
+    # --- IMPLEMENTATION GOES HERE ---
+    i=0
+    while i < size:
+        randomlist = random.sample(range(0, 100), size)
+        matrix.append(randomlist)
+        i += 1
+    # --------------------------------
+
+    return matrix
+
+
+
+def uoc_hill_cipher(message, key):
+    """
+    EXERCISE 2: Hill cipher
+    :message: message to cipher (plaintext)
+    :key: key to use when ciphering the message (as it is returned by 
+          uoc_hill_genkey() )
+    :return: ciphered text
+    """
+
+    ciphertext = ""
+
+    #### IMPLEMENTATION GOES HERE ####
+
+
+
+    # --------------------------------
+
+    return ciphertext
+
+
+def uoc_hill_decipher(message, key):
+    """
+    EXERCISE 3: Hill decipher
+    :message: message to decipher (ciphertext)
+    :key: key to use when deciphering the message (as it is returned by 
+          uoc_hill_genkey() )
+    :return: plaintext corresponding to the ciphertext
+    """
+
+    plaintext = ""
+
+    #### IMPLEMENTATION GOES HERE ####
+
+
+
+
+    # --------------------------------
+
+    return plaintext
+
+
+
+if __name__ == '__main__':
+
+    # my own examples
+    matrix= uoc_hill_genkey(2)
+    print(matrix)
+
+
+
+
+
+
